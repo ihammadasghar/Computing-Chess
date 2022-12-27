10fb3e398ad8fdc6ea11d81ed6dea48d24108ef8
Hammad Asghar <ihammadasghar@gmail.com>
2022-12-27 16:19:12 +0000

    Fixed prunning

--- minimax.py
+++ minimax.py
@@ -4,6 +4,7 @@ def minimax(position, destination, game_board, max_player, alpha=float("-inf"), 
     board = Board()
     board.set_state(game_board)
     board.make_move(position, destination)
+    
     if depth == 0 or board.game_over:
         return board.state
 
@@ -12,7 +13,7 @@ def minimax(position, destination, game_board, max_player, alpha=float("-inf"), 
         for position, destination in board.min_player_moves:
             board_state = minimax(position, destination, board, False, alpha, beta, depth-1)
             min_state = min(min_state, board_state)
-            beta = min(beta, board_state)
+            beta = min(beta, min_state)
             if beta <= alpha:
                 break
         return min_state
@@ -22,7 +23,7 @@ def minimax(position, destination, game_board, max_player, alpha=float("-inf"), 
         for position, destination in board.max_player_moves:
             board_state = minimax(position, destination, board, True, alpha, beta, depth-1)
             max_state = max(max_state, board_state)
-            alpha = max(alpha, board_state)
+            alpha = max(alpha, max_state)
             if beta <= alpha:
                 break
         return max_state
